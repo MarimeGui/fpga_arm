ac3671805fc1c7462ad18ef05c8174e64b5d75a6
MarimeGui <lepro.guillaume@gmail.com>
2024-09-24 20:06:35 +0200

    Create dir on assembly

--- assembly/assemble.sh
+++ assembly/assemble.sh
@@ -2,6 +2,7 @@
 
 # Assemble program to object file, raw binary machine code, and to textual hex file
 
+mkdir -p out
 arm-none-eabi-as -march=armv7 program.s -o out/program.o
 arm-none-eabi-objcopy -O binary -j .text out/program.o out/program.bin
 python to_hex.py out/program.bin out/program.hex
