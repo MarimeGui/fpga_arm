ddb79fd25e40d96ea44e9c08d5053b834ca40c2c
fetohiaras <65745388+fetohiaras@users.noreply.github.com>
2024-10-22 11:19:48 -0300

    Integration of ALU and register file + testbench

---
+++ verilog/alureg/aluregs_tb.v
@@ -0,0 +1,78 @@
+`timescale 1ns/1ns
+
+module aluregs_tb();
+    reg clock;
+    reg [31:0] in_reg, pc_in;
+    reg [3:0] sel_in, sel_p0, sel_p1;
+    reg [3:0] flags_in;
+    wire [31:0] p0, p1, pc_out;
+    wire [3:0] flags_out;
+    reg [31:0] LHS, RHS;
+    reg [4:0] uop;
+    wire [31:0] out_alu;
+    wire [3:0] flags; // [Z, C, N, V]
+
+    regs UUT_A (
+        .clock(clock),
+        .in_reg(in_reg),
+        .pc_in(pc_in),
+        .sel_in(sel_in),
+        .sel_p0(sel_p0),
+        .sel_p1(sel_p1),
+        .flags_in(flags_in),
+        .p0(p0),
+        .p1(p1),
+        .pc_out(pc_out),
+        .flags_out(flags_out)
+    );
+
+    ALU UUT_B (
+        .LHS(LHS),
+        .RHS(RHS),
+        .uop(uop),
+        .out_alu(out_alu),
+        .flags(flags)
+    );
+
+    initial begin
+        clock = 0;
+        forever #5 clock = ~clock; // Clock toggles every 5ns -> period of 10ns
+    end
+
+    initial begin
+        $dumpfile("aluregs_tb.vcd"); 
+        $dumpvars(0, aluregs_tb);
+        #10;
+
+        in_reg = 32'h2; // Data in r0
+        sel_in = 4'b0000;
+        #10;
+
+        in_reg = 32'h1; // Data in r1
+        sel_in = 4'b0001;
+        #10;
+
+        sel_p0 = 4'b0001; // Operation: r1 - r0 stored in r2
+        sel_p1 = 4'b0000;
+        sel_in = 4'b0010;
+        #10; 
+
+        LHS <= p0;
+        RHS <= p1;
+        uop <= 5'b00010;  // SUB operation
+        in_reg <= out_alu;
+        #10; 
+
+        sel_in = 4'b0011;
+        sel_p0 = 4'b0010; // Change p0 to r2 to view operation result
+        #10;
+
+        $display("P0 value (R2): %h", out_alu);
+
+        flags_in <= flags;
+        #10;
+        $display("Flags: %b", flags_in);
+    end
+
+
+endmodule
---
+++ verilog/alureg/regs.v
@@ -0,0 +1,28 @@
+module regs (
+    input clock,
+    input [31:0] in_reg, pc_in,
+    input [3:0] sel_in, sel_p0, sel_p1,
+    input [3:0] flags_in,
+    output reg [31:0] p0, p1, pc_out, 
+    output reg [3:0] flags_out
+);
+
+reg [31:0] regs [15:0]; // Internal register file, r0 - r15 (r13 = SP, r14 = LR, r15 = PC)
+reg [3:0] flags;        // Internal flags register
+
+// Register read operation
+always @ (negedge clock) begin
+    flags_out <= flags; 
+    pc_out <= regs[15];  // PC = r15       
+    p0 <= regs[sel_p0];  // Read register selected by sel_p0
+    p1 <= regs[sel_p1];  // Read register selected by sel_p1
+end
+
+// Register write operation
+always @ (posedge clock) begin
+    regs[sel_in] <= in_reg;     // Write to the register selected by sel_in
+    flags <= flags_in;      // Update flags register
+    regs[15] <= pc_in;      // Update PC (r15) with pc_in
+end
+
+endmodule
