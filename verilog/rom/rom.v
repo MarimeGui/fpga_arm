b73009cb82d7754d7e27fb66e060d9c9e8577c98
Nicolas HAAS <nicolas.haas@netc.fr>
2024-11-12 16:06:54 +0100

    module ROM with the right size of rom memory

--- verilog/rom/rom.v
+++ verilog/rom/rom.v
@@ -1,8 +1,9 @@
-
-// Memory's name: ROM-Flash
-// Size: 1 MBytes = 1 048 576 Bytes
-// Start address: 0x0800 0000
-// End address: 0x080F FFFF
+/*
+Memory's name: ROM-Flash
+Size: 1 MBytes = 1 048 576 Bytes
+Start address: 0x0800 0000
+End address: 0x080F FFFF
+*/
 
 module rom(
 	input clock, write_enable,
@@ -10,14 +11,14 @@ module rom(
 	output reg [31:0] data_out
 );
 
-reg [31:0] ram_block [1048576:0]; // 0x10 0000 = 1048576
+reg [31:0] rom_block [1048575:0]; // 0xF FFFF = 1048575
 
 always @(posedge clock) begin
 	if ( address[31:20] == 12'h080 ) begin // Check if the address is for the ROM, so if its start by 0x080
         if(write_enable)
-            ram_block[address[19:0]] <= data_in; // Write
+            rom_block[address[19:0]] <= data_in; // Write
         else
-            data_out <= ram_block[address[19:0]]; // Read
+            data_out <= rom_block[address[19:0]]; // Read
 	end
 end
 
