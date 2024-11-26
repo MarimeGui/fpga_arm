bf4dc506f207144e1b3ca8331fe56b4b8bf3f7eb
HAASNicolas <nicolas.haas@netc.fr>
2024-11-26 15:04:54 +0100

    Append enable in reg_file

--- verilog/execute/reg_file/regs_tb.v
+++ verilog/execute/reg_file/regs_tb.v
@@ -2,6 +2,7 @@
 
 module regs_tb();
     reg clock;
+	reg in_enable;
     reg [31:0] in_reg;
     reg [3:0] sel_in, sel_p0, sel_p1;
     reg [3:0] flags_in;
@@ -11,6 +12,7 @@ module regs_tb();
     // Instantiate the regs module
     regs UUT (
         .clock(clock),
+		.in_enable(in_enable),
         .in_reg(in_reg),
         .sel_in(sel_in),
         .sel_p0(sel_p0),
@@ -34,6 +36,7 @@ module regs_tb();
         $dumpvars(0, regs_tb);
 
         // Initialize inputs
+		in_enable <= 1;
         in_reg = 32'h00000000;
         sel_in = 4'b0000; 
         sel_p0 = 4'b0000;
@@ -63,12 +66,20 @@ module regs_tb();
         sel_p0 = 4'b0000; // Select r0
         sel_p1 = 4'b0001; // Select r1
         #10;
+		
+		// Step 5: Disable reg & Write to r0
+		in_enable <= 0; // Disable write
+        in_reg = 32'h12345678; // Write to r15 (PC)
+        sel_in = 1'b0; // Select r0
+		sel_p0 = 1'b1; // Select r1
+        #10;
 
-        // Step 5: Read from PC (via pc_out)
+        // Step 6: Read from PC (via pc_out)
         #10; // PC is automatically updated in pc_out
+        $display("PC_out: %h", pc_out);
         
-        // Step 6: Read flags (via flags_out)
-        #10; 
+        // Step 7: Read flags (via flags_out)
+        $display("Flags: %b", flags_in);
 
         // Add more test cases as needed, such as writing to r2-r12, and testing the stack pointer (r13) and link register (r14).
         
