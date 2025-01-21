771730d5ab37a64f3de515cc6bc5b28610fc4457
HAASNicolas <nicolas.haas@netc.fr>
2025-01-21 16:15:10 +0100

    Module Fetch

--- verilog/fetch/fetch.sv
+++ verilog/fetch/fetch.sv
@@ -1,19 +1,15 @@
 module Fetch(
     input clk,
-
-    // Connects to memory data bus
-    input [31:0] data,
-    // 16-bit LE instruction
-    output reg [15:0] instruction
+	input [31:0] delta_i,
+	output reg [31:0] index
 );
 
-// The part that controls access to memory is responsible for fetching PC from Register File and making sure the memory data bus contains the next instruction to execute.
-// Also, the Register File is responsible for increasing PC each cycle.
-// THe advantage of doing this is that if an instruction modifies PC, the new value will be taken into account when the negative edge falls.
+initial begin
+	index = 10;
+end
 
 always @(posedge clk) begin
-    // Keep highest two bytes, reverse order
-    instruction <= data[15:8] | (data[7:0] << 8);
+    index += 1 + delta_i;
 end
 
 endmodule
--- verilog/fetch/fetch_tb.v
+++ verilog/fetch/fetch_tb.v
@@ -2,67 +2,37 @@
 
 module fetch_tb();
     reg clk;
-
-    wire [31:0] data_from_rom;
-    reg [31:0] data_to_rom;
-    reg [31:0] address;
-    wire [15:0] instruction;
-
-    reg write_enable;
-
+	reg [31:0] delta_i;
+	wire [31:0] index;
+	
     Fetch UUT(
         .clk(clk),
-        .data(data_from_rom),
-        .instruction(instruction)
-    );
-
-    rom TestRom(
-        .clock(clk),
-		.write_enable(write_enable),
-		.address(address),
-		.data_in(data_to_rom),
-		.data_out(data_from_rom)
+        .delta_i(delta_i),
+        .index(index)
     );
 
     // Setup clock
     initial begin
-        clk = 1;
+        clk = 0;
         forever #5 clk = ~clk;
     end
 
     initial begin
-        // Wait for a full cycle to make display clearer
-        #10;
-
-        // Write some data in Rom
-        data_to_rom = 32'h67452301; // Data mimicking Little Endian instructions
-		write_enable = 1;
-		address = 32'h08000010;
+        $dumpfile("fetch_tb.vcd");
+        $dumpvars(0, fetch_tb);
+		
+		delta_i = 0;
 		#10;
-
-        // Write some data in Rom again
-        data_to_rom = 32'hDDCCBBAA;
-		write_enable = 1;
-		address = 32'h08000014;
 		#10;
-
-        // Set all signals to Hi-Z
-        data_to_rom = 32'bZ;
-		write_enable = 1'bZ;
-		address = 32'bZ;
-        write_enable = 0;
-        #10;
-
-        // Try to read this data
-        address = 32'h08000010;
-        #10;
-        address <= address + 2;
-        #10;
-        address <= address + 2;
-        #10;
-        address <= address + 2;
-        #10;
-        address <= address + 2;
+		
+		delta_i = 10;
+		#10;
+		
+		delta_i = -5;
+		#10;
+		#10;
+		
+		$stop;
     end
 
 endmodule
