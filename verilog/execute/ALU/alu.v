7e3db48039e94dfdb82f07327f9d70f5e125488a
fetohiaras <theo.farias@gmail.com>
2025-02-11 14:11:13 +0100

    Changed ALU behavior on NOP

--- verilog/execute/ALU/alu.v
+++ verilog/execute/ALU/alu.v
@@ -7,10 +7,16 @@ module ALU(
 );
 
 always @(*) begin
-    // Reset flags
-    flags = 4'b0000;
+    if (uop != 5'b00000) begin
+        // Reset flags
+        flags = 4'b0000;
+    end
 
     case(uop)
+        5'b00000: begin // NOP
+            out_alu = 0;
+        end
+
         5'b00001: begin  // ADD
             {flags[1], out_alu} = LHS + RHS; // Set Carry flag (C)
             // Overflow flag (for signed addition): Overflow occurs if signs of LHS and RHS are the same, but result sign differs.
@@ -35,14 +41,16 @@ always @(*) begin
         5'b01000: out_alu = RHS;  // MOV 
     endcase
     
-    // Set the Negative flag (N) based on MSB of the result
-    flags[2] = out_alu[31];  
+    if (uop != 5'b00000) begin
+        // Set the Negative flag (N) based on MSB of the result
+        flags[2] = out_alu[31];  
 
-    // Set the Zero flag (Z)
-    if (out_alu == 32'b0) 
-        flags[0] = 1'b1;  // Zero flag is set if result is zero
-    else 
-        flags[0] = 1'b0;
+        // Set the Zero flag (Z)
+        if (out_alu == 32'b0) 
+            flags[0] = 1'b1;  // Zero flag is set if result is zero
+        else 
+            flags[0] = 1'b0;
+    end
 end
 
 endmodule
