e9ea1020f667d463721f169321b727a31b9477a5
MarimeGui <lepro.guillaume@gmail.com>
2025-03-11 16:15:18 +0100

    Fix DCache testbench

--- verilog/execute/dcache/dcache_tb.v
+++ verilog/execute/dcache/dcache_tb.v
@@ -2,13 +2,13 @@
 
 module dcache_tb();
     reg clock;
-    reg [4:0] addr;
+    reg [31:0] addr;
     reg [31:0] data_in;
-    reg [3:0] uop;
+    reg [4:0] uop;
     wire [31:0] data_out;
 
-    parameter [3:0] STR_UOP = 4'b1001;
-    parameter [3:0] LDR_UOP = 4'b1010;
+    parameter [4:0] STR_UOP = 4'b1001;
+    parameter [4:0] LDR_UOP = 4'b1010;
 
     dcache #(
         .STR_UOP(STR_UOP), // Adds the parameters to the instantiated module
@@ -27,35 +27,36 @@ module dcache_tb();
     end
 
     initial begin
-        $dumpfile("dcache_tb.vcd");
-        $dumpvars(0, dcache_tb);
-
-        addr <= 5'b11111;   
+        // Not a valid uOP, should not change anything
+        addr <= 5'b11111;
         data_in <= 32'hDEADBEEF;
-        uop <= 4'b0010;     
-        #10; 
+        uop <= 4'b0010;
+        #10;
 
-        $display("1) uop = 4'b0010 -> data_out = %h (expected Z)", data_out);
-
-        addr <= 5'b01010;   
+        // Store some data
+        addr <= 5'b01010;
         data_in <= 32'h12345678;
-        uop <= STR_UOP;     
-        #10; 
-
-       
-        uop <= LDR_UOP;    
-        #10; 
-        $display("2) Reading from addr %b, data_out = %h (expected 0x12345678)", addr, data_out);
-
-        addr <= 5'b00101;  
-        data_in <= 32'hAABBCCDD;
-        uop <= STR_UOP; 
+        uop <= STR_UOP;
+        $display("1) uop = 4'b0010 -> data_out = %h (expected zeroes)", data_out);
         #10;
 
+        // Retrieve it without changing parameters
+        data_in <= 32'h0;
         uop <= LDR_UOP;
+        $display("2) Reading from addr %b, data_out = %h (expected 0x12345678)", addr, data_out);
         #10;
 
+        // Store some other data
+        addr <= 5'b00101;
+        data_in <= 32'hAABBCCDD;
+        uop <= STR_UOP;
+        #10;
+
+        // Retrieve it too
+        data_in <= 32'h0;
+        uop <= LDR_UOP;
         $display("3) reading from addr %b, data_out = %h (expected 0xAABBCCDD)", addr, data_out);
+        #10;
 
         $stop;
     end
