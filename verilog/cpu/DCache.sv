5e3835ff747d0b4c800f43c69c78a374cccec838
HAASNicolas <nicolas.haas@netc.fr>
2025-03-19 07:42:04 +0100

    Fix DCache issue with Vivado

--- verilog/cpu/DCache.sv
+++ verilog/cpu/DCache.sv
@@ -14,31 +14,6 @@ module DCache (
 // This holds exactly 32 values of 32 bits, this means we need (theoretically) 5 bits for addressing
 bit [31:0] dcache_block [31:0];
 
-<<<<<<< HEAD:verilog/cpu/DCache.sv
-always @(posedge clock) begin
-    case (uop)
-        LDR: begin
-            // LDR: Reads data stored on addr to data_out
-            data_out <= dcache_block[addr];
-        end
-
-        default: begin
-            // Output 0 if not LDR for rising edge
-            data_out <= 32'b0;
-        end
-    endcase
-end
-
-always @(negedge clock) begin
-    case (uop)
-        STR: begin
-            // STR: Stores data on addr
-            dcache_block[addr] <= data_in;
-        end
-
-        default: begin
-            // Don't do anything if not STR for falling edge
-=======
 bit last_clock_state;
 
 initial begin
@@ -50,7 +25,7 @@ always @(*) begin
         last_clock_state <= clock;
         if (last_clock_state) begin
             // Posedge of clock
-            if (uop == LDR_UOP)
+            if (uop == LDR)
                 // LDR: Reads data stored on addr to data_out
                 data_out <= dcache_block[addr];
             else
@@ -58,13 +33,10 @@ always @(*) begin
                 data_out <= 32'b0;
         end else begin
             // Negedge of clock
-            if (uop == STR_UOP)
+            if (uop == STR)
                 // STR: Stores data on addr
                 dcache_block[addr] <= data_in;
-            else
-                // 0 output if neither STR nor LDR 
-                data_out <= 32'b0;
->>>>>>> 204ee144deabd129d3a29624fc780ef49c632e99:verilog/cpu/dcache.sv
+            // Don't do anything if not STR for falling edge
         end
     end
 end
