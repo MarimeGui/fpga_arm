a663f949305114de9915c3228a77927cbd7d9c19
MarimeGui <lepro.guillaume@gmail.com>
2025-03-24 23:41:44 +0100

    Assembled output was wrong without proper label

--- assembly/program.s
+++ assembly/program.s
@@ -2,12 +2,13 @@
 .global _start
 
 _start:
+begin:
     MOV R1, #32 // Gpio address
-    MOV R2, #0  // Chaser
-    MOV R3, #0 //Chaser counter
-loop:    
+    MOV R2, #0 // Chaser
+    MOV R3, #0 // Chaser counter
+loop:
     CMP R3,#32
-    BEQ _start 
+    BEQ begin
     
     LSL R2,#1
     CMP R3,#16
@@ -15,7 +16,7 @@ loop:
     ADD R2,#1
 skip:
     ADD R3,#1
-        
+    
     STR R2,[R1,#0]
     B loop
 halt:
