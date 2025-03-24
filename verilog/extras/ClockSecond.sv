b257d8b821de4832ef776c4c7f2b506872088fad
MarimeGui <lepro.guillaume@gmail.com>
2025-03-24 23:46:56 +0100

    Used for demonstration on board

---
+++ verilog/extras/ClockSecond.sv
@@ -0,0 +1,23 @@
+// https://www.fpga4student.com/2017/08/verilog-code-for-clock-divider-on-fpga.html
+
+module ClockSecond #(
+    parameter DIVISOR = 32'd100000000
+) (
+    input clk_in,
+    output bit clk_out,
+);
+
+bit[31:0] counter=32'd0;
+
+always @(posedge clk_in)
+begin
+    counter <= counter + 32'd1;
+
+    if(counter>=(DIVISOR-1))
+        counter <= 32'd0;
+    
+    clk_out <= (counter<DIVISOR/2)?1'b1:1'b0;
+
+end
+
+endmodule
